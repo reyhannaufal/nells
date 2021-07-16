998f567da23c9457a87ea3f82d21c2020fb905f0
reyhannaufal <reyhannaufal21@gmail.com>
2021-07-16 16:27:34 +0800

    feat: add active link to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,16 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react';
 import { Popover, Transition } from '@headlessui/react';
-import {
-    ChartBarIcon,
-    CursorClickIcon,
-    MenuIcon,
-    RefreshIcon,
-    ShieldCheckIcon,
-    ViewGridIcon,
-    XIcon,
-} from '@heroicons/react/outline';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { useRouter } from 'next/dist/client/router';
+import Link from 'next/link';
 
 const solutions = [
     {
         name: 'Home',
         description:
             'Get a better understanding of where your traffic is coming from.',
-        href: '#',
+        href: '/',
     },
     {
         name: 'Product',
@@ -33,17 +27,18 @@ const solutions = [
         name: 'Glow Up!',
         description:
             "Connect with third-party tools that you're already using.",
-        href: '#',
+        href: '/glowup',
     },
     {
         name: 'Health Zone',
         description:
             'Build strategic funnels that will drive your customers to convert',
-        href: '#',
+        href: '/healthzone',
     },
 ];
 
 export default function Header() {
+    const router = useRouter();
     return (
         <Popover className='relative z-20 bg-white font-primary'>
             {({ open }) => (
@@ -51,14 +46,16 @@ export default function Header() {
                     <div className='px-4 mx-auto shadow-md sm:px-6'>
                         <div className='flex flex-row-reverse items-center justify-between py-6 md:flex-row md md:justify-start md:space-x-10'>
                             <div className='flex justify-start w-7/12 lg:w-0 lg:flex-1'>
-                                <a href='#'>
-                                    <span className='sr-only'>Nells</span>
-                                    <img
-                                        className='w-auto h-8 sm:h-10'
-                                        src='/svg/colored-logo.svg'
-                                        alt='colored-logo'
-                                    />
-                                </a>
+                                <Link href='/'>
+                                    <a>
+                                        <span className='sr-only'>Nells</span>
+                                        <img
+                                            className='w-auto h-8 sm:h-10'
+                                            src='/svg/colored-logo.svg'
+                                            alt='colored-logo'
+                                        />
+                                    </a>
+                                </Link>
                             </div>
                             <div className='-my-2 -mr-2 md:hidden'>
                                 <Popover.Button className='inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
@@ -73,36 +70,59 @@ export default function Header() {
                                 as='nav'
                                 className='hidden space-x-10 md:flex'
                             >
-                                <a
+                                <Link href='/'>
+                                    <a
+                                        className={`${
+                                            router.pathname === '/'
+                                                ? 'font-bold'
+                                                : 'font-medium'
+                                        } text-base  text-gray-900 hover:text-gray-900`}
+                                    >
+                                        Home
+                                    </a>
+                                </Link>
+                                <Link
                                     href='#'
-                                    className='text-base font-bold text-gray-900 hover:text-gray-900'
-                                >
-                                    Home
-                                </a>
-                                <a
-                                    href='#'
-                                    className='text-base font-medium text-gray-500 hover:text-gray-900'
+                                    className={`${
+                                        router.pathname === '/product'
+                                            ? 'font-bold'
+                                            : 'font-medium'
+                                    } text-base  text-gray-900 hover:text-gray-900`}
                                 >
                                     Product
-                                </a>
-                                <a
+                                </Link>
+                                <Link
                                     href='#'
-                                    className='text-base font-medium text-gray-500 hover:text-gray-900'
+                                    className={`${
+                                        router.pathname === '/aboutus'
+                                            ? 'font-bold'
+                                            : 'font-medium'
+                                    } text-base  text-gray-900 hover:text-gray-900`}
                                 >
                                     About Us
-                                </a>
-                                <a
-                                    href='#'
-                                    className='text-base font-medium text-gray-500 hover:text-gray-900'
-                                >
-                                    Glow Up!
-                                </a>
-                                <a
-                                    href='#'
-                                    className='text-base font-medium text-gray-500 hover:text-gray-900'
-                                >
-                                    Health Zone
-                                </a>
+                                </Link>
+                                <Link href='/glowup'>
+                                    <a
+                                        className={`${
+                                            router.pathname == '/glowup'
+                                                ? 'font-bold'
+                                                : 'font-medium'
+                                        } text-base  text-gray-900 hover:text-gray-900`}
+                                    >
+                                        Glow Up!
+                                    </a>
+                                </Link>
+                                <Link href='healthzone'>
+                                    <a
+                                        className={`${
+                                            router.pathname == '/healthzone'
+                                                ? 'font-bold'
+                                                : 'font-medium'
+                                        } text-base  text-gray-900 hover:text-gray-900`}
+                                    >
+                                        Health Zone
+                                    </a>
+                                </Link>
                             </Popover.Group>
                         </div>
                     </div>
@@ -147,15 +167,25 @@ export default function Header() {
                                     <div className='mt-6'>
                                         <nav className='grid gap-y-8'>
                                             {solutions.map((item) => (
-                                                <a
+                                                <Link
                                                     key={item.name}
                                                     href={item.href}
-                                                    className='flex items-center p-3 -m-3 rounded-md hover:bg-gray-50'
                                                 >
-                                                    <span className='ml-3 text-base font-medium text-gray-900'>
-                                                        {item.name}
-                                                    </span>
-                                                </a>
+                                                    <a
+                                                        className={`${
+                                                            router.pathname ===
+                                                            item.href
+                                                                ? 'font-bold'
+                                                                : 'font-medium'
+                                                        } flex items-center p-3 -m-3 rounded-md hover:bg-gray-50`}
+                                                    >
+                                                        <span
+                                                            className={`ml-3 text-base text-gray-900`}
+                                                        >
+                                                            {item.name}
+                                                        </span>
+                                                    </a>
+                                                </Link>
                                             ))}
                                         </nav>
                                     </div>
