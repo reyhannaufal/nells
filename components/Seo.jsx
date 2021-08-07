9e24a8d311ee853133fce3f29d15246fca7a547a
reyhannaufal <reyhannaufal21@gmail.com>
2021-08-08 03:34:54 +0800

    feat: change google font to dispaly swap

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -68,11 +68,13 @@ export default function Seo(props) {
                 crossorigin
             />
             <link
-                href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=optional'
+                rel='preload'
+                href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap'
                 rel='stylesheet'
             />
             <link
-                href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=optional'
+                rel='preload'
+                href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap'
                 rel='stylesheet'
             />
         </Head>
