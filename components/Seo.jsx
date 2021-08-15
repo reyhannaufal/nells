e7d7d2c97446b527d0c87bcff13efaa9b900d60e
reyhannaufal <reyhannaufal21@gmail.com>
2021-08-15 18:55:17 +0800

    fix: seo structure

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Seo(props) {
     const router = useRouter();
@@ -14,71 +15,74 @@ export default function Seo(props) {
     };
 
     return (
-        <Head>
-            <Html lang='id' />
-            <title>{meta.title}</title>
+        <Html lang='id'>
+            <Head>
+                <title>{meta.title}</title>
 
-            <meta name='robots' content={meta.robots} />
-            <meta content={meta.description} name='description' />
-            <meta
-                property='og:url'
-                content={`https://nells.vercel.app/${router.asPath}`}
-            />
-            <link
-                rel='canonical'
-                href={`https://nells.vercel.app/${router.asPath}`}
-            />
-            {/* Open Graph */}
-            <meta property='og:type' content={meta.type} />
-            <meta property='og:site_name' content='Nells' />
-            <meta property='og:description' content={meta.description} />
-            <meta property='og:title' content={meta.title} />
-            <meta name='image' property='og:image' content={meta.image} />
-            {/* Twitter */}
-            <meta name='twitter:card' content='summary_large_image' />
-            <meta name='twitter:site' content='@nellsofficial.id' />
-            <meta name='twitter:title' content={meta.title} />
-            <meta name='twitter:description' content={meta.description} />
-            <meta name='twitter:image' content={meta.image} />
-            {meta.date && (
-                <>
-                    <meta
-                        property='article:published_time'
-                        content={meta.date}
-                    />
-                    <meta
-                        name='publish_date'
-                        property='og:publish_date'
-                        content={meta.date}
-                    />
-                    <meta
-                        name='author'
-                        property='article:author'
-                        content='Reyhan Naufal'
-                    />
-                </>
-            )}
-            <link
-                rel='canonical'
-                href={`https://nellsofficial.com//${router.asPath}`}
-            />
-            <link rel='icon' href='/favicon.png' />
-            <link rel='preconnect' href='https://fonts.googleapis.com' />
-            <link
-                rel='preconnect'
-                href='https://fonts.gstatic.com'
-                crossorigin
-            />
-            <link
-                rel='preload'
-                href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap'
-                rel='stylesheet'
-            />
-            <link
-                rel='preload'
-                href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap'
-                rel='stylesheet'
-            />
-        </Head>
+                <meta name='robots' content={meta.robots} />
+                <meta content={meta.description} name='description' />
+                <meta
+                    property='og:url'
+                    content={`https://nells.vercel.app/${router.asPath}`}
+                />
+                <link
+                    rel='canonical'
+                    href={`https://nells.vercel.app/${router.asPath}`}
+                />
+                {/* Open Graph */}
+                <meta property='og:type' content={meta.type} />
+                <meta property='og:site_name' content='Nells' />
+                <meta property='og:description' content={meta.description} />
+                <meta property='og:title' content={meta.title} />
+                <meta name='image' property='og:image' content={meta.image} />
+                {/* Twitter */}
+                <meta name='twitter:card' content='summary_large_image' />
+                <meta name='twitter:site' content='@nellsofficial.id' />
+                <meta name='twitter:title' content={meta.title} />
+                <meta name='twitter:description' content={meta.description} />
+                <meta name='twitter:image' content={meta.image} />
+                {meta.date && (
+                    <>
+                        <meta
+                            property='article:published_time'
+                            content={meta.date}
+                        />
+                        <meta
+                            name='publish_date'
+                            property='og:publish_date'
+                            content={meta.date}
+                        />
+                        <meta
+                            name='author'
+                            property='article:author'
+                            content='Reyhan Naufal'
+                        />
+                    </>
+                )}
+                <link
+                    rel='canonical'
+                    href={`https://nellsofficial.com//${router.asPath}`}
+                />
+                <link rel='icon' href='/favicon.png' />
+                <link rel='preconnect' href='https://fonts.googleapis.com' />
+                <link
+                    rel='preconnect'
+                    href='https://fonts.gstatic.com'
+                    crossorigin
+                />
+                <link
+                    rel='preload'
+                    href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap'
+                    rel='stylesheet'
+                />
+                <link
+                    rel='preload'
+                    href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap'
+                    rel='stylesheet'
+                />
+            </Head>
+            <Main />
+            <NextScript />
+        </Html>
     );
 }
